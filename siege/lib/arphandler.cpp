c86ca8e1a06a01210835400361115f7456793c87
eliad <eliadbz@gmail.com>
2020-11-11 00:35:55 +0200

    removed arphandler.cpp

--- siege/lib/arphandler.cpp
+++
@@ -1,9 +0,0 @@
-#include "arphandler.hpp"
-
-ARPHandler::ARPHandler(std::string *hw_victim, std::string *hw_target, std::string *ip_victim, std::string *ip_target){
-    this->hw_victim = *hw_victim;
-    this->hw_target = *hw_target;
-    this->ip_victim = *ip_victim;
-    this->ip_target = *ip_target;
-    *this->to_stop = false;
-}
